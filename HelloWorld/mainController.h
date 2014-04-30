2ab7cc61fda0a93ab91d7f9ee97598975bad870c
Dylan <Dylan@192-168-1-2.tpgi.com.au>
2014-04-30 22:38:58 +1000

    first commit

---
+++ HelloWorld/mainController.h
@@ -0,0 +1,13 @@
+//
+//  mainController.h
+//  HelloWorld
+//
+//  Created by Dylan on 30/04/2014.
+//  Copyright (c) 2014 ABC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainController : UIViewController
+
+@end
